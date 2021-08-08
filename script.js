098db74b6de2f4c2ed34654ef0c26a9ee70e6381
HisanCmt <safiechowdhury2004@gmail.com>
2021-08-08 18:51:48 -0400

    eighth commit

--- script.js
+++ script.js
@@ -34,7 +34,151 @@ let pkmn4 = {
     FilePath: "images/crustle.png"
 }
 
-let pkmnArray = [pkmn1, pkmn2, pkmn3, pkmn4];
+let pkmn5 = {
+    Pokemon: "Gardevoir",
+    Role: "Attacker & Ranged",
+    Difficulty: "Intermediate",
+    Best_Stats: "Offense",
+    Description: "Gardevoir strikes its opponents from far away with its mysterious psychic power. It has a lot of moves with unique movements, but they are very powerful when used effectively.",
+    FilePath: "images/gardevoir.png"
+}
+
+let pkmn6 = {
+    Pokemon: "Cramorant",
+    Role: "Attacker & Ranged",
+    Difficulty: "Expert",
+    Best_Stats: "Offense",
+    Description: "Cramorant is the Gulp Pokémon. Cramorant’s Ability, Gulp Missile, allows it to attack by spitting out prey it catches with Dive.",
+    FilePath: "images/cramorant.png"
+}
+
+let pkmn7 = {
+    Pokemon: "Talonflame",
+    Role: "Speedster & Melee",
+    Difficulty: "Novice",
+    Best_Stats: "Mobility",
+    Description: "Talonflame toys with the opposing team by using its moves to quickly maneuver itself and fly over obstacles.",
+    FilePath: "images/talonflame.png"
+}
+
+let pkmn8 = {
+    Pokemon: "Machamp",
+    Role: "All-Rounder & Melee",
+    Difficulty: "Intermediate",
+    Best_Stats: "Offense",
+    Description: "Machamp is a Pokémon that specializes in melee combat, powered by its strong fighting moves.",
+    FilePath: "images/machamp.png"
+}
+
+let pkmn9 = {
+    Pokemon: "Charizard",
+    Role: "All-Rounder & Melee",
+    Difficulty: "Novice",
+    Best_Stats: "Offense",
+    Description: "When it’s still a Charmander, it’s not the most reliable, but when it evolves into Charizard, it can use its powerful moves to attack and deal damage over a wide area.",
+    FilePath: "images/charizard.png"
+}
+
+let pkmn10 = {
+    Pokemon: "Mr.Mime",
+    Role: "Supporter & Melee",
+    Difficulty: "Intermediate",
+    Best_Stats: "Endurance & Support",
+    Description: "Mr. Mime specializes in hindering opposing Pokémon with its tricky moves.",
+    FilePath: "images/mime.png"
+}
+
+let pkmn11 = {
+    Pokemon: "Blastoise",
+    Role: "Soon",
+    Difficulty: "Soon",
+    Best_Stats: "Soon",
+    Description: "Soon",
+    FilePath: "images/blastoise.png"
+}
+
+let pkmn12 = {
+    Pokemon: "Gengar",
+    Role: "Speedster & Melee",
+    Difficulty: "Expert",
+    Best_Stats: "Mobility",
+    Description: "Gengar, a Pokémon that lurks in the shadows, is a master of surprise attacks.",
+    FilePath: "images/gengar.png"
+}
+
+let pkmn13 = {
+    Pokemon: "Slowbro",
+    Role: "Defender & Ranged",
+    Difficulty: "Intermediate",
+    Best_Stats: "Endurance & Support",
+    Description: "No matter the situation, Slowbro goes about life at its own pace. With its characteristic high defense and blissful unawareness, Slowbro is unfazed by opponents’ attacks.",
+    FilePath: "images/slowbro.png"
+}
+
+let pkmn14 = {
+    Pokemon: "Alolan Ninetales",
+    Role: "Attacker & Ranged",
+    Difficulty: "Intermediate",
+    Best_Stats: "Offense",
+    Description: "Alolan Ninetales uses moves that freeze its opponents in ice, making them unable to move or attack.",
+    FilePath: "images/ninetales.png"
+}
+
+let pkmn15 = {
+    Pokemon: "Venasaur",
+    Role: "Attacker & Ranged",
+    Difficulty: "Intermediate",
+    Best_Stats: "Offense",
+    Description: "If its moves land successfully, Venusaur can deal a huge amount of damage to its opponents, even from far away.",
+    FilePath: "images/venasaur.png"
+}
+
+let pkmn16 = {
+    Pokemon: "Eldegoss",
+    Role: "Supporter & Ranged",
+    Difficulty: "Novice",
+    Best_Stats: "Support",
+    Description: "Eldegoss is a master supporter that can restore its allies’ HP and boost their movement speed.",
+    FilePath: "images/eldegoss.png"
+}
+
+let pkmn17 = {
+    Pokemon: "Cinderace",
+    Role: "Attacker & Ranged",
+    Difficulty: "Novice",
+    Best_Stats: "Offense",
+    Description: "Cinderace doesn’t just have high attack power—it’s also got the speed to drive opponents crazy.",
+    FilePath: "images/cinderace.png"
+}
+
+let pkmn18 = {
+    Pokemon: "Pikachu",
+    Role: "Attacker & Ranged",
+    Difficulty: "Novice",
+    Best_Stats: "Offense",
+    Description: "Pikachu is a Pokémon that excels at attacking opponents from far away with electricity. Sometimes the attacks even leave opposing Pokémon paralyzed!",
+    FilePath: "images/pikachu.png"
+}
+
+let pkmn19 = {
+    Pokemon: "Greninja",
+    Role: "Attacker & Ranged",
+    Difficulty: "Expert",
+    Best_Stats: "Offense",
+    Description: "Greninja is a Pokémon that uses smokescreens and illusory copies of itself to hide.",
+    FilePath: "images/greninja.png"
+}
+
+let pkmn20 = {
+    Pokemon: "Lucario",
+    Role: "All-Rounder & Melee",
+    Difficulty: "Expert",
+    Best_Stats: "Mobility",
+    Description: "Lucario is a well-balanced Pokémon that combines speed and attack power.",
+    FilePath: "images/lucario.png"
+}
+
+let pkmnArray = [pkmn1, pkmn2, pkmn3, pkmn4, pkmn5, pkmn6, pkmn7, pkmn8, pkmn9, pkmn10, pkmn11, pkmn12, pkmn13, pkmn14, pkmn15, pkmn16, pkmn17, pkmn18, pkmn19, pkmn20];
 
 let pokemon = document.getElementById("pokemon-details");
 
